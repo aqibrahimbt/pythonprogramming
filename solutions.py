c9ed239022f53f1e351d3e3b0efcda6d12f59fe7
Abdul Qadir Ibrahim <abdulqadr.ibrahim@stud.uni-goettingen.de>
2020-07-27 21:51:00 +0200

    new solutions

---
+++ solutions.py
@@ -0,0 +1,82 @@
+## Question 1
+
+keys = ['Ten', 'Twenty', 'Thirty', 'Forty']
+values = [10, 20, 30, 40]
+my_dictionary = dict(zip(keys, values))
+#print(my_dictionary)
+
+
+## Question 2 
+dict1 = {'Ten':10, 'Twenty':20, 'Thirty':30}
+dict2 = {'Thirty':30, 'Fourty':40, 'Fifty':50}
+#(Option 1) # Advanced feature 
+dict3 = {**dict1, **dict2}
+#print(dict3)
+
+# (Option 2)
+dict3 = dict1.copy()
+dict3.update(dict2)
+#print(dict3)
+
+
+## Question 3
+listOne = [3, 6, 9, 12, 15, 18, 21]
+listTwo = [4, 8, 12, 16, 20, 24, 28]
+
+listThree = list()
+oddElements = listOne[1::2] # odd index
+evenElements = listTwo[0::2] # even index
+#print('odd elements: ' + str(oddElements))
+#print('even elements: ' + str(evenElements))
+listThree.extend(oddElements)
+listThree.extend(evenElements)
+#print(listThree)
+
+
+## Question 4
+val_list = [11, 45, 8, 11, 23, 45, 23, 45, 89]
+
+count = dict() #initialize dict
+for number in val_list:
+    #print(number)
+    if (number in count):
+        #print('the number is in the dictionary already')
+        count[number] += 1 # update the value using the key(in this case: number)
+    else:
+        #print('This number is not in the dictionary. so we add')
+        count[number] = 1 # Create the key-value in the dictionary
+
+
+## Question 5
+aList = [1, 2, 3, 4, 5, 6, 7]
+aList = [x * x for x in aList] # creates the for-loop in the list directly
+#print(aList)
+
+
+## Question 6
+## TODO: Try trnaslation table
+'''
+delimeter = " "
+f = delimeter.join(nulist)
+str1 = "a"
+str2 = "1"
+str3 = ""
+tab = f.maketrans(str1, str2, str3)
+f.translate(tab)
+'''
+## TODO: Check the other options for filter function
+list1 = ["Mike", "", "Emma", "Kelly", "", "Brad"]
+new_list = list(filter(None, list1))
+#print(new_list)
+
+
+## Question 7
+def calculate(number):
+    print(number)
+    if number:
+        return number + calculate(number - 1)
+    else:
+        return 0
+
+result = calculate(100)
+print('total result: ', result)
---
+++ test.py
@@ -0,0 +1,29 @@
+ ### ( Dictionary) Task 1: Goal is to convert this to a dictionary:  
+ #keys = ['Ten', 'Twenty', 'Thirty'] 
+ #values = [10, 20, 30]
+
+
+### (Dictionary) Task 2: Merge the following two python dictionaries into one 
+#dict1 = {'Ten':10, 'Twenty':20, 'Thirty':30}
+#dict2 = {'Thirty':30, 'Fourty':40, 'Fifty':50}
+
+
+### Task 3 - Given a two list. Create a third list by picking an odd-index element from the first list and even index elements from second.
+#listOne = [3, 6, 9, 12, 15, 18, 21]
+#listTwo = [4, 8, 12, 16, 20, 24, 28]
+
+
+### Task 4: Given a list iterate it and count the occurrence of each element and create a dictionary to show the count of each element
+#Original list  [11, 45, 8, 11, 23, 45, 23, 45, 89]
+#Printing count of each item   {11: 2, 45: 3, 8: 1, 23: 2, 89: 1}
+
+
+### Task 5: Given a Python list. Turn every item of a list into its square
+#aList = [1, 2, 3, 4, 5, 6, 7]
+
+
+### Task 6: Remove empty strings from the list of strings
+#list1 = ["Mike", "", "Emma", "Kelly", "", "Brad"]
+
+
+### Task 7: Write a recursive function to calculate the sum of numbers from 0 to 10
